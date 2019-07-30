11ce20def49db463ca495f4efeb7c5415bf6c2c4
Garrison28 <ghighsmith28@gmail.com>
2019-07-29 18:39:10 -0700

    Homework Complete

--- clock.js
+++ clock.js
@@ -1,26 +1,28 @@
 
 // declaring my constant variables
-const hourHand = document.querySelector("#hour");
-const minuteHand = document.querySelector("#minute");
-const secondHand = document.querySelector("#second");
+var hourHand = document.getElementById("hour");
+var minuteHand = document.getElementById("minute");
+var secondHand = document.getElementById("second");
 
 
 function clock(){
-    //var now resets clock after 24 hrs
-var now = new Date();
-var hour = now.getHours();
-var minute = now.getMinutes();
-var second = now.getSeconds();
+    //var new date provides real time
+var curDate = new Date();
+console.log(curDate);
+let hour = curDate.getHours();
+let minute = curDate.getMinutes();
+let second = curDate.getSeconds();
 // console.log("Hour: " + hr + "Minute: " + min + "Second: " + sec);
 // declaring my let variables for the keeping time algorthim
 let hrPosition = hour * 360/12 + ((minute * 360/60)/12);
 let minPosition = (minute * 360/60) + (second * 360/60)/60;
 let secPosition = second * 360/60;
 //declaring that the hr, sec, and min hand will roatate
-//Example; the hr hand will rotate at a rate of the var hour times 360/12 plus the var minute times 360/60/12
+//Example: the hr hand will rotate at a rate of the var hour times 360/12 plus the var minute times 360/60/12* 
+//*set at the variable hour which set to local time
 
-hourHand.style.transform = "roatate(" + hrPosition + "deg)";
-minuteHand.style.transform = "roatate(" + minPosition + "deg)";
+hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+minuteHand.style.transform = "rotate(" + minPosition + "deg)";
 secondHand.style.transform = "rotate(" + secPosition + "deg)";
 
 }
